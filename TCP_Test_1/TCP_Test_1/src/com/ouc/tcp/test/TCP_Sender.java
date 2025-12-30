e8a9229ad1782629db5d8b7929d30cacddcf08e8
Wei <1956886771a@gmail.com>
2025-12-30 14:07:48 +0800

    Update TCP_Sender.java

--- TCP_Test_1/TCP_Test_1/src/com/ouc/tcp/test/TCP_Sender.java
+++ TCP_Test_1/TCP_Test_1/src/com/ouc/tcp/test/TCP_Sender.java
@@ -1,28 +1,30 @@
 package com.ouc.tcp.test;
 
-import java.util.LinkedList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.TimerTask;
+import java.util.TreeMap;
 
 import com.ouc.tcp.client.TCP_Sender_ADT;
 import com.ouc.tcp.client.UDT_Timer;
 import com.ouc.tcp.message.*;
 
-public class TCP_Sender_GBN extends TCP_Sender_ADT {
+public class TCP_Sender_SR extends TCP_Sender_ADT {
 	
 	private static final int WINDOW_SIZE = 10;
-	private LinkedList<TCP_PACKET> sendWindow = new LinkedList<TCP_PACKET>();
+	private TreeMap<Integer, TCP_PACKET> sendWindow = new TreeMap<Integer, TCP_PACKET>();
+	private Map<Integer, UDT_Timer> timerMap = new HashMap<Integer, UDT_Timer>();
 	private int windowBase = 1;
 	private int nextSeqNum = 1;
-	private UDT_Timer timer;
 	
 	//构造函数
-	public TCP_Sender_GBN() {
+	public TCP_Sender_SR() {
 		super();
 		super.initTCP_Sender(this);
 	}
 	
 	@Override
-	//可靠发送：滑动窗口+单计时器（超时重传窗口内所有未确认包）
+	//可靠发送：滑动窗口 + 每个包独立计时器
 	public void rdt_send(int dataIndex, int[] appData) {
 		
 		//窗口满则等待
@@ -39,54 +41,59 @@ public class TCP_Sender_GBN extends TCP_Sender_ADT {
 		tcpH.setTh_seq(seq);
 		tcpS.setData(appData);
 		TCP_PACKET tcpPack = new TCP_PACKET(tcpH, tcpS, destinAddr);
-		tcpH.setTh_sum(CheckSum_GBN.computeChkSum(tcpPack));
+		tcpH.setTh_sum(CheckSum_SR.computeChkSum(tcpPack));
 		tcpPack.setTcpH(tcpH);
 		
 		//发送
 		udt_send(tcpPack);
 		
-		//入窗口
+		//入窗口并启动独立计时器
 		try {
-			sendWindow.add(tcpPack.clone());
+			sendWindow.put(seq, tcpPack.clone());
 		} catch (CloneNotSupportedException e) {
 			e.printStackTrace();
 		}
-		
-		//如是窗口第一个包，启动计时器
-		if(windowBase == seq) {
-			startTimer();
-		}
+		startTimer(seq);
 		
 		nextSeqNum++;
 	}
 	
-	private void startTimer() {
-		cancelTimer();
-		timer = new UDT_Timer();
-		timer.schedule(new TimerTask() {
+	private void startTimer(final int seq) {
+		cancelTimer(seq);
+		UDT_Timer t = new UDT_Timer();
+		timerMap.put(seq, t);
+		t.schedule(new TimerTask() {
 			@Override
 			public void run() {
-				//超时：重传窗口内所有未确认包
-				for(TCP_PACKET packet : sendWindow) {
-					udt_send(packet);
+				//重传该序号的包
+				TCP_PACKET p = sendWindow.get(seq);
+				if(p != null) {
+					udt_send(p);
+					startTimer(seq);
 				}
-				//重启计时器
-				startTimer();
 			}
 		}, 3000);
 	}
 	
-	private void cancelTimer() {
-		if(timer != null) {
-			timer.cancel();
-			timer = null;
+	private void cancelTimer(int seq) {
+		UDT_Timer t = timerMap.remove(seq);
+		if(t != null) {
+			t.cancel();
+		}
+	}
+	
+	private void updateWindowBase() {
+		if(sendWindow.isEmpty()) {
+			windowBase = nextSeqNum;
+		} else {
+			windowBase = sendWindow.firstKey();
 		}
 	}
 	
 	@Override
 	//不可靠发送：设置错误标志
 	public void udt_send(TCP_PACKET stcpPack) {
-		//允许综合错误测试
+		//综合错误测试
 		tcpH.setTh_eflag((byte)7);
 		client.send(stcpPack);
 	}
@@ -96,20 +103,11 @@ public class TCP_Sender_GBN extends TCP_Sender_ADT {
 	public void waitACK() {
 		if(!ackQueue.isEmpty()) {
 			int ackNum = ackQueue.poll();
-			//累计确认：ackNum 表示已正确收到的最后序号
-			if(ackNum >= windowBase) {
-				//移除窗口中已确认的包
-				while(!sendWindow.isEmpty() && sendWindow.peek().getTcpH().getTh_seq() <= ackNum) {
-					sendWindow.poll();
-					windowBase = ackNum + 1;
-				}
-				
-				//窗口非空，重启计时器；否则取消
-				if(!sendWindow.isEmpty()) {
-					startTimer();
-				} else {
-					cancelTimer();
-				}
+			//收到该序号的ACK，则移除并取消计时器
+			if(sendWindow.containsKey(ackNum)) {
+				sendWindow.remove(ackNum);
+				cancelTimer(ackNum);
+				updateWindowBase();
 			}
 		}
 	}
@@ -117,7 +115,7 @@ public class TCP_Sender_GBN extends TCP_Sender_ADT {
 	@Override
 	//接收ACK报文
 	public void recv(TCP_PACKET recvPack) {
-		if(CheckSum_GBN.computeChkSum(recvPack) == recvPack.getTcpH().getTh_sum()) {
+		if(CheckSum_SR.computeChkSum(recvPack) == recvPack.getTcpH().getTh_sum()) {
 			ackQueue.add(recvPack.getTcpH().getTh_ack());
 			waitACK();
 		}
